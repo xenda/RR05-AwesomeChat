b022d8b6d68616d32b016f3106ffdaa4c21851ee
Alvaro Pereyra <alvaro@xendacentral.com>
2012-01-09 18:45:46 -0500

    Adds default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 AwesomeChat::Application.routes.draw do
   resources :messages
+  root :to => "messages#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
