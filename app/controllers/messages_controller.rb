fb6f4b1cdd0dacd67b10c0cf0d8eed0e9794814a
Alvaro Pereyra <alvaro@xendacentral.com>
2012-01-09 19:15:22 -0500

    Updates with Pusher code

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -44,7 +44,6 @@ class MessagesController < ApplicationController
     respond_to do |format|
       if @message.save
         format.html { redirect_to @message, notice: 'Message was successfully created.' }
-        format.js # create.js.erb
         format.json { render json: @message, status: :created, location: @message }
       else
         format.html { render action: "new" }
