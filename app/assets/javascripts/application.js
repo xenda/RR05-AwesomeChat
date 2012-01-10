38a6a30c6b5580e80e94f4ef5ad07ee0eeea733a
Alvaro Pereyra <alvaro@xendacentral.com>
2012-01-09 19:51:02 -0500

    Adds oembed support

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,31 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+$(function() {
+  updateContentForOembed("#chat_container");
+});
+
+function updateContentForOembed(element){
+  // We capture the Message content
+  text = $(element).html();
+
+  // If there are URLs on it, we changed them to links
+  regMatches = (text.match(/\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.])(?:[^\s()<>]+|\([^\s()<>]+\))+(?:\([^\s()<>]+\)|[^`!()\[\]{};:'".,<>?«»“”‘’\s]))/gi));
+  if (regMatches) {
+
+  // Iterate through each link found and convert it to link
+  $.each(regMatches, function(index,value){
+      reg_value = value.replace("?","\\?");
+      var re = new RegExp("\\s*"+reg_value+"\\s*","gim");
+      text = text.replace(re,"<a href='"+ value +"' class='oembed'>"+value+"</a>");
+   
+  });
+
+  $(element).html(text);
+  $(element + " .oembed").oembed();
+
+  }
+
+}
